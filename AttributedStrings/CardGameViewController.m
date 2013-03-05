d709e339739ae801f8adbe961f63bdf5d1e49cc2
blackcrisz <cristiano_4yu@yahoo.com>
2013-03-05 19:32:28 +0000

    First commit

--- AttributedStrings/CardGameViewController.m
+++ AttributedStrings/CardGameViewController.m
@@ -9,7 +9,11 @@
 #import "CardGameViewController.h"
 
 @interface CardGameViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *labetText;
+@property (weak, nonatomic) IBOutlet UILabel *labelWord;
+@property (weak, nonatomic) IBOutlet UIStepper *stepper;
 
+- (NSArray*) numberOfWordsInLabel;
 @end
 
 @implementation CardGameViewController
@@ -18,6 +22,63 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self changeWord];
+}
+
+- (NSArray *)numberOfWordsInLabel
+{
+    NSArray *wordList = [[self.labetText.attributedText string] componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    if ([wordList count])
+    {
+        return wordList;
+    }
+    else
+    {
+        return @[@""];
+    }
+}
+
+- (NSString*) selectedWord
+{
+    return [self numberOfWordsInLabel][(int)self.stepper.value];
+}
+
+- (IBAction)changeWord
+{
+    self.stepper.maximumValue = [self numberOfWordsInLabel].count - 1;
+    self.labelWord.text = [self selectedWord];
+}
+
+- (void)addTextAttributes:(NSDictionary*)attributes range:(NSRange)range
+{
+    if (range.location != NSNotFound)
+    {
+        NSMutableAttributedString *mat = [self.labetText.attributedText mutableCopy];
+        [mat addAttributes:attributes range:range];
+        
+        self.labetText.attributedText = mat;
+    }
+}
+
+- (void)addSelectedWordAttributes:(NSDictionary*)attributes
+{
+    NSRange wordRange = [[self.labetText.attributedText string] rangeOfString:self.labelWord.text];
+    [self addTextAttributes:attributes range:wordRange];
+}
+
+- (IBAction)underline
+{
+
+    [self addSelectedWordAttributes:@{NSUnderlineStyleAttributeName : @(NSUnderlineStyleSingle)}];
+}
+
+- (IBAction)nounderline
+{
+    [self addSelectedWordAttributes:@{NSUnderlineStyleAttributeName : @(NSUnderlineStyleNone)}];
+}
+- (IBAction)addColor:(UIButton*)sender
+{
+    [self addSelectedWordAttributes:@{NSForegroundColorAttributeName : sender.backgroundColor}];
 }
 
 - (void)didReceiveMemoryWarning
